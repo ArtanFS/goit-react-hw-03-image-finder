ec793bb4bd412759aa9e56796616da8cd07bfcb1
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2023-11-19 23:40:06 +0200

    make search component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import Searchbar from './Searchbar';
 
 export class App extends Component {
-  state = {};
+  state = {
+    images: [],
+    query: '',
+    page: 1,
+    isLoading: false,
+  };
+
+  onSubmit = searchData => {
+    this.setState({ query: searchData });
+  };
 
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <Searchbar onSubmit={this.onSubmit} />
+      </div>
+    );
   }
 }
--- src/components/Searchbar/index.jsx
+++ src/components/Searchbar/index.jsx
@@ -1,21 +1,44 @@
-const Searchbar = ({ onSubmit }) => {
-  return (
-    <header class="searchbar">
-      <form class="form">
-        <button type="submit" class="button">
-          <span class="button-label">Search</span>
-        </button>
+import { Component } from 'react';
 
-        <input
-          class="input"
-          type="text"
-          autocomplete="off"
-          autofocus
-          placeholder="Search images and photos"
-        />
-      </form>
-    </header>
-  );
-};
+class Searchbar extends Component {
+  state = {
+    search: '',
+  };
+
+  handleChange = e => {
+    this.setState({ search: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    if (this.state.search.trim() === '') {
+      return;
+    }
+    this.props.onSubmit(this.state.search);
+    this.setState({ search: '' });
+  };
+
+  render() {
+    return (
+      <header className="searchbar">
+        <form className="form" onSubmit={this.onSubmit}>
+          <button type="submit" className="button">
+            <span className="button-label">Search</span>
+          </button>
+
+          <input
+            className="input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={this.state.search}
+            onChange={this.handleChange}
+          />
+        </form>
+      </header>
+    );
+  }
+}
 
 export default Searchbar;
